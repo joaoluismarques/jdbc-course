384b367e6b94ae58e301abd15f376151172f5acd
Joao Luis <joaolu19@outlook.com>
2021-09-18 23:19:20 -0300

    Department and Seller entities Classes

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import model.entites.Department;
+import model.entites.Seller;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+
+		
+	}
+
+}
