a55392ceff732032ff9fdbe4c4c8711de76b9a4e
Joao Luis <joaolu19@outlook.com>
2021-09-19 19:16:53 -0300

    Implementations done!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Stream;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -17,10 +17,21 @@ public class Program {
 		
 		List<Seller> list = sellerDao.findAll();
 		for(Seller obj : list) {
-			System.out.println(obj);
+		 System.out.println(obj);
 		}
+		System.out.println("==== TEST 2:  seller insert ========");
+		Department department = new Department(3, "Fashion");
+		Seller newSeller = new Seller(null, "james", "james@gmail.com", new Date(), 3250.0, department );
+		sellerDao.insert(newSeller);
+		System.out.println("New id = " + newSeller.getId());
+		System.out.println("New name  = " + newSeller.getDepartment());
 		
+		System.out.println("==== TEST 3:  seller update ========");
+		Seller updateSeller = new Seller(9, "James", "james@gmail.com", new Date(), 4000.0, department);
+		 sellerDao.update(updateSeller);
 		
+		System.out.println("==== TEST 4:  seller delete ========");
+		sellerDao.deleteById(9);
 	}
 
 }
